39547692ebfab220d0b20741221f4de7e8e75807
buddhamagnet <buddhamagnet@gmail.com>
2016-03-07 20:57:40 +0000

    complete americanize

--- examples/americanize/main.go
+++ examples/americanize/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 var (
-	inFile  io.ReadCloser
-	outFile io.ReadWriteCloser
+	inFile          io.ReadCloser
+	outFile         io.ReadWriteCloser
+	wordRx          *regexp.Regexp
+	replacer        func(string) string
+	britishAmerican string
 )
+
+func init() {
+	wordRx = regexp.MustCompile("[A-Za-z]+")
+	britishAmerican = "british-american.txt"
+}
 
 func fileOpen(filename string) {
 	var err error
@@ -52,9 +65,64 @@ func main() {
 }
 
 func filenamesFromCommandLine() (inFilename, outFilename string, err error) {
-	return "sample.txt", "output.txt", nil
+	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
+		return "", "", fmt.Errorf("usage: %s [<]infile.txt [>]outfile.txt", filepath.Base(os.Args[0]))
+	}
+	if len(os.Args) > 1 {
+		inFilename = os.Args[1]
+		if len(os.Args) > 2 {
+			outFilename = os.Args[2]
+		}
+	}
+	if inFilename != "" && inFilename == outFilename {
+		log.Fatal("cannot overwrite input file")
+	}
+	return inFilename, outFilename, nil
 }
 
-func americanize(inFile io.Reader, outFile io.Writer) error {
+func americanize(inFile io.Reader, outFile io.Writer) (err error) {
+	reader := bufio.NewReader(inFile)
+	writer := bufio.NewWriter(outFile)
+	defer writer.Flush()
+	if replacer, err = makeReplaceFunction(britishAmerican); err != nil {
+		return err
+	}
+	eof := false
+	for !eof {
+		var line string
+		line, err = reader.ReadString('\n')
+		if err == io.EOF {
+			err = nil
+			eof = true
+		} else if err != nil {
+			return err
+		}
+		line = wordRx.ReplaceAllStringFunc(line, replacer)
+		if _, err = writer.WriteString(line); err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func makeReplaceFunction(file string) (func(string) string, error) {
+	rawBytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	text := string(rawBytes)
+	usForBritish := make(map[string]string)
+	lines := strings.Split(text, "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) == 2 {
+			usForBritish[fields[0]] = fields[1]
+		}
+	}
+	return func(word string) string {
+		if usWord, found := usForBritish[word]; found {
+			return usWord
+		}
+		return word
+	}, nil
 }
