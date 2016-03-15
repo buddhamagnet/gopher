e09d5f5ba5524f360c348a400447460214707fd1
buddhamagnet <buddhamagnet@gmail.com>
2016-03-15 13:51:21 +0000

    build out stats server

--- pig/examples/stats/main.go
+++ pig/examples/stats/main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
 
-	"github.com/buddhamagnet/gopher/pig/examples/stats/web"
+	"github.com/gorilla/mux"
 )
 
+type Page struct{}
+
 func main() {
-	web.NewRouter()
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	r := mux.NewRouter()
+	staticHandler := http.FileServer(http.Dir("./public/"))
+	http.Handle("/public/", http.StripPrefix("/public/", staticHandler))
+	r.HandleFunc("/", Root)
+	http.Handle("/", r)
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// Root is the hypermedia root endpoint of the service (GET).
+func Root(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, &Page{})
 }
